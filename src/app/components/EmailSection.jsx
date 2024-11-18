158ca43163a9c172318627a7bc133a5ba1dd4e45
Bh Mohan Varma <110345847+mohanvarma22@users.noreply.github.com>
2024-11-18 17:50:50 +0530

    trying to deploy

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,13 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/mohanvarma22">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://linkedin.com/in/
+mohan-varma-bh
+
+">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
